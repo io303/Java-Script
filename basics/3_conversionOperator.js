d9e3e6c75b1805899bab835ec896f65501208046
io303 <srahul12768@gmail.com>
2025-08-31 20:47:37 +0530

    Update 3_conversionOperator.js

--- basics/3_conversionOperator.js
+++ basics/3_conversionOperator.js
@@ -24,4 +24,41 @@ let someNumber =33
 let stringNumber=String(someNumber)
 
 
+console.log(stringNumber);
+
+
+//****************  OPERATION */
+let value=3;
+let negvalue=-value
+console.log(negvalue);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log((3+4)*5%3);
+console.log(+true);// confusion it  will give 1 
+
+console.log(+"");// same it convert 
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
 
