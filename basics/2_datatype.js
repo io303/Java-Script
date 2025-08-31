5fa28db506ba2d302ce9614a1524760bbf135d98
io303 <srahul12768@gmail.com>
2025-08-31 17:19:05 +0530

    Update 2_datatype.js

---
+++ basics/2_datatype.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) we are using nodejs not the browser
+
+let name="rahul" // string "  "
+let age=19 //number 
+let isLoggedIn=false // boolean 
+let state; // undefined  
+let place=null
+
+
+console.log(typeof name);
+console.log(typeof age); // 2 to power of 53 limit 
+console.log(typeof isLoggedIn);
+console.log(typeof place);
+
+// bidint 
+// null ==>> object 
+// undefined ==>> undefined 
+// symbol ==>> unique 
+
