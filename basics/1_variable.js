62b51dcc1a892bf706d97c59bc33cca789c05b83
io303 <srahul12768@gmail.com>
2025-08-31 16:37:03 +0530

    learn basics of js

---
+++ basics/1_variable.js
@@ -0,0 +1,12 @@
+const accountId=13232
+let accountEmail="hitesh@gmail.com"
+var  accountPassword ="1234"
+let accountState;
+console.log(accountState);// undefined type 
+console.log(accountEmail);//  email
+
+
+// we prefer not use of var because of issue inblock scope and functional scope
+
+console.table([accountEmail,accountId,accountPassword,accountState])
+
