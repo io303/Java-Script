f928f878ebfed1769a5bc8cfa03f6f1302ee9503
io303 <srahul12768@gmail.com>
2025-08-31 17:51:49 +0530

    Create 3_conversionOperator.js

---
+++ basics/3_conversionOperator.js
@@ -0,0 +1,27 @@
+let score ="33"
+console.log(typeof score);
+
+let valueNumber=Number(score)
+console.log(valueNumber)  // string to number
+console.log(typeof valueNumber);// its value is Nan but if 33 or numerical then it will give the value 
+
+
+// "33"==>>  33
+//"33abc"==> NaN
+// true==> 1& false => 0
+
+let isLoggedIn ="rahul"
+let booleanIsLOggedIn= Boolean(isLoggedIn)
+
+console.log(booleanIsLOggedIn);
+
+// 1=>true , 0=> false
+// " "=> false
+// "rahul"=> true
+
+
+let someNumber =33
+let stringNumber=String(someNumber)
+
+
+
