52203712c57424c6e40cb37792c9c1e6bb9bb6fb
io303 <srahul12768@gmail.com>
2025-09-01 20:46:23 +0530

    basic of array

--- basics2/2_array.js
+++ basics2/2_array.js
@@ -9,8 +9,26 @@ const dc=["batman",'superman',"flash"]
 // marvel.concat(dc)
 // console.log(marvel);// same as earlier
 
-const aLLHero=marvel.concate(dc)
-console.log(aLLHero);
+const aLLHero=marvel.concat(dc)
+// console.log(aLLHero);
+
+const all_new_heros= [...marvel, ...dc]// best way forpusing 
+// console.log(all_new_heros);
+
+const another_array=[1,2,3,4,[4,5,6,7],[9,0,[4,5]]]
+const real_another_array=another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Hitesh"))
+console.log(Array.from("hitesh"));
+console.log(Array.from({name:"hitesh"}));
+
+let a1=100
+let a2=200
+let a3=300
+console.log(Array.of(a1,a2,a3));
+
 
 
 
