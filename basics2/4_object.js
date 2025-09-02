e829d33f50a526959d12719b6e1dab6268089365
io303 <srahul12768@gmail.com>
2025-09-02 10:49:56 +0530

    Update 4_object.js

--- basics2/4_object.js
+++ basics2/4_object.js
@@ -6,5 +6,24 @@ const tinder={}// non singlrton object
  tinder.IsLoggedIn=false
 
 
- console.log(tinder);
+//  console.log(tinder);
  
+const regular_user ={
+    email:"somename@gmail.com",
+    fullname:{
+        userName:{
+            firstname:"rahul",
+            lastname:"Singh"
+        }
+    }
+}
+// console.log(regular_user); // whole object will preinted 
+
+console.log(regular_user.fullname.firstname);
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj3={5:"a",6:"b"}
+
+constobj3={obj1,obj2}
+console.log(obj3);
+
