702144c8fa83f4525f17b90bb4c6c25c8f98f1c6
io303 <srahul12768@gmail.com>
2025-09-02 14:13:25 +0530

    Update 4_object.js

--- basics2/4_object.js
+++ basics2/4_object.js
@@ -22,8 +22,52 @@ const regular_user ={
 console.log(regular_user.fullname.firstname);
 const obj1={1:"a",2:"b"}
 const obj2={3:"a",4:"b"}
-const obj3={5:"a",6:"b"}
+// const obj3={5:"a",6:"b"}
+const obj4 ={5:"a",6:'b'}
 
-constobj3={obj1,obj2}
-console.log(obj3);
+// constobj3={obj1,obj2}
+// const obj3=Object.assign({},obj1,obj2,obj4)
+// console.log(obj3);
+
+const obj3={ ...obj1,...obj2}
+// console.log(obj3);
+
+
+const users=[
+    {
+        id:1,
+        email:"h@gmail.com"
+    },
+    {
+        id:2,
+        email:"e@gmail.com"
+    },
+    {
+        id:3,
+        email:"f@gmail.com"
+    }
+
+]
+users[1].email 
+
+console.log(tinder);
+
+// console.log(Object.keys(tinder));// give the keys in array 
+console.log(Object.values(tinder));
+console.log(Object.entries);
+
+console.log(tinder.hasOwnProperty('isLoggedIn'));
+
+const course ={
+    coursename:"js in hindi",
+    price:'999',
+    courseInstructor: "hitesh"
+}
+
+// console.log(course.courseInstructor);
+const {courseInstructor:Instructor}=course 
+console.log(Instructor);
+
+
+
 
